dcc60ae9526c9b6de8b67def48ebad0c9526e1d5
Pavel Parkhomey <pavelpark@aol.com>
2017-06-14 13:33:54 -0700

    Gitignore added

---
+++ PokemonGORadar/PokemonGORadar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonGORadar
+//
+//  Created by Pavel Parkhomey on 6/14/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
