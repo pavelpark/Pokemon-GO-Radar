e6ef3c5aaf1500c4c1457f8070db5029149ed213
Pavel Parkhomey <pavelpark@aol.com>
2017-06-15 15:23:11 -0700

    Created bridging header from Objective-C to Swift to install geoFire.

---
+++ PokemonGORadar/PokemonGORadar/PokemonGoFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokemonGoFinder-Bridging-Header.h
+//  PokemonGORadar
+//
+//  Created by Pavel Parkhomey on 6/15/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import "GeoFire.h"
